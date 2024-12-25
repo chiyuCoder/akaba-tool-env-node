d1ba89b8bfd1aa83a30eeb134611e309f60136d6
chiyuCoder <stevindjimmy@gmail.com>
2024-12-25 17:05:00 +0800

    path-helper

--- src/util-helper/path-helper.ts
+++ src/util-helper/path-helper.ts
@@ -1,15 +1,110 @@
-import { NodePath, NodeProcess } from "./node-helper";
-
-let projectRootPath: string = NodeProcess.cwd();
+import { NodeFs, NodePath, NodeProcess } from "./node-helper";
+let projectRootPath: string = "";
 
 export function setProjectRootPath(dirPath: string) {
   projectRootPath = dirPath;
 }
 
+/**
+ * @since 0.0.4
+ */
+export function setProjectRootPathWithCwd() {
+  setProjectRootPath(NodeProcess.cwd());
+}
+
+/**
+ * @description
+ * -- when return empty string, it will not call "setProjectRootPath"
+ * @since 0.0.4
+ */
+export function setProjectRootPathWithTargetFileName(
+  currentPath: string, 
+  fileName: string
+): string {
+  const targetPath = getParentPathUntilHas(currentPath, fileName);
+  if (targetPath) {
+    setProjectRootPath(targetPath);
+  }
+  return targetPath;
+}
+
 export function getPathUnderProjectRoot(...pathParts: string[]): string {
-  return NodePath.join(projectRootPath, ...pathParts);
+  if (projectRootPath) {
+    return NodePath.join(projectRootPath, ...pathParts);
+  }
+  /**
+   * @since 0.0.4
+   */
+  throw new Error("please call setProjectRootPath manually");
+}
+
+/**
+ * @since 0.0.4
+ */
+export function getPosixPathUnderProjectRoot(...pathParts: string[]): string {
+  const systemPath = getPathUnderProjectRoot(...pathParts);
+  return toPosixPath(systemPath);
+}
+
+/**
+ * @since 0.0.4
+ */
+export function joinPath(...pathParts: string[]): string {
+  return toPosixPath(NodePath.join(...pathParts));
+}
+
+/**
+ * @since 0.0.4
+ */
+export function resolvePath(...pathParts: string[]): string {
+  return toPosixPath(NodePath.resolve(...pathParts));
+}
+
+/**
+ * @since 0.0.4
+ */
+export function getDirPath(fullPath: string): string {
+  return toPosixPath(NodePath.dirname(fullPath));
+}
+
+export function formatToPath(inputInfo: NodePath.FormatInputPathObject): string {
+  return toPosixPath(NodePath.format(inputInfo));
+}
+/**
+ * @since 0.0.4
+ */
+export function getRelativePath(from: string, to: string) {
+  return toPosixPath(NodePath.relative(from, to));
+}
+
+/**
+ * @since 0.0.4
+ */
+export function toPosixNormalPath(pathPart: string): string {
+  return toPosixPath(NodePath.normalize(pathPart));
 }
 
 export function toPosixPath(fullPath: string): string {
   return fullPath.replace(/\\{1,2}/g, "/");
+}
+
+/**
+ * @since 0.0.4
+ * @param currentPath 
+ * @param targetFileName 
+ * @returns 
+ */
+export function getParentPathUntilHas(
+  currentPath: string,
+  targetFileName: string
+): string {
+  const targetFilePath = NodePath.join(currentPath, targetFileName);
+  if (NodeFs.existsSync(targetFilePath)) {
+    return targetFilePath;
+  }
+  const parentDirPath = NodePath.dirname(currentPath);
+  if (NodePath.normalize(parentDirPath) === NodePath.normalize(currentPath)) {
+    return "";
+  }
+  return getParentPathUntilHas(parentDirPath, targetFileName);
 }
